0c37a330afd30d49ea3e7169cfc3e5d9c592835a
Giovani Gonzalez <darkgeat@gmail.com>
2021-11-04 10:04:12 -0600

    checking if report is generated and run script from files, running script

--- ci/scripts/emulator.sh
+++ ci/scripts/emulator.sh
@@ -10,8 +10,8 @@ function config_emulator_settings() {
 
 function wait_emulator_to_be_ready() {
   adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
-  emulator -avd "test" -verbose -no-boot-anim -no-snapshot-save -no-window -accel off -gpu auto -skin 1440x2880 &
-  boot_completed=true
+  emulator -avd "test" -verbose -no-boot-anim -no-snapshot-save -no-window -accel auto -gpu auto -skin 1440x2880 -noaudio &
+  boot_completed=false
   while [ "$boot_completed" == false ]; do
     status=$(adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
     echo "Boot Status: $status"
