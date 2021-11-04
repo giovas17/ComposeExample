4e99a87dbe024b849f89b77421133126a6c77d63
Giovani Gonzalez <darkgeat@gmail.com>
2021-11-04 10:35:07 -0600

    checking if report is generated and run script from files, running script wth no acceleration 4

--- ci/scripts/emulator.sh
+++ ci/scripts/emulator.sh
@@ -12,19 +12,18 @@ function wait_emulator_to_be_ready() {
   adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
   emulator -avd "test" -verbose -no-boot-anim -no-snapshot-save -no-window -accel off -gpu off -skin 1440x2880 -noaudio &
   boot_completed=false
-  adb wait-for-device shell <<ENDSCRIPT
-      echo -n "Waiting for device to boot "
-      echo "" > /data/local/tmp/zero
-      getprop dev.bootcomplete > /data/local/tmp/bootcomplete
-      while cmp /data/local/tmp/zero /data/local/tmp/bootcomplete; do
-      {
-          echo -n "."
-          sleep 1
-          getprop dev.bootcomplete > /data/local/tmp/bootcomplete
-      }; done
-      echo "Booted."
-      exit
-  ENDSCRIPT
+  adb wait-for-device shell
+    echo -n "Waiting for device to boot "
+    echo "" > /data/local/tmp/zero
+    getprop dev.bootcomplete > /data/local/tmp/bootcomplete
+    while cmp /data/local/tmp/zero /data/local/tmp/bootcomplete; do
+    {
+        echo -n "."
+        sleep 1
+        getprop dev.bootcomplete > /data/local/tmp/bootcomplete
+    }; done
+    echo "Booted."
+    exit
 
   echo "Waiting 30 secs for us to be really booted"
   sleep 30
