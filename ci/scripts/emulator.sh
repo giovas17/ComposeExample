efcf8be63312519b66af3db8d72ed4ba7064f15d
Giovani Gonzalez <darkgeat@gmail.com>
2021-11-04 18:45:14 -0600

    checking if report is generated and run script from files, running script wth no acceleration 7

--- ci/scripts/emulator.sh
+++ ci/scripts/emulator.sh
@@ -10,26 +10,22 @@ function config_emulator_settings() {
 
 function wait_emulator_to_be_ready() {
   adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
-  emulator -avd "test" -verbose -no-boot-anim -no-snapshot-save -no-window -accel off -gpu off -skin 1440x2880 -noaudio -debug-all &
+  adb root
+  adb shell avbctl disable-verification
+  adb disable-verity
+  emulator -avd "test" -verbose -no-boot-anim -no-snapshot -no-window -accel off -gpu off -skin 1440x2880 -noaudio -debug-all &
   boot_completed=false
-  adb wait-for-device shell
-    echo -n "Waiting for device to boot "
-    echo "" > /data/local/tmp/zero
-    getprop dev.bootcomplete > /data/local/tmp/bootcomplete
-    while cmp /data/local/tmp/zero /data/local/tmp/bootcomplete; do
-    {
-        echo -n "."
-        sleep 1
-        getprop dev.bootcomplete > /data/local/tmp/bootcomplete
-    }; done
-    echo "Booted."
-    exit
+  while [ "$boot_completed" == false ]; do
+    status=$(adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
+    echo "Boot Status: $status"
 
-  echo "Waiting 30 secs for us to be really booted"
-  sleep 30
-
-  echo "Unlocking screen"
-  adb shell "input keyevent 82"
+    if [ "$status" == "1" ]; then
+      boot_completed=true
+      echo "Emulator successfully started!"
+    else
+      sleep 1
+    fi
+  done
 }
 
 function start_emulator_if_possible() {
