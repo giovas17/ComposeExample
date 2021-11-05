01c78cc138316b7f64a4c1970e24aba3edac20ff
Giovani Gonzalez <darkgeat@gmail.com>
2021-11-04 18:52:31 -0600

    checking if report is generated and run script from files, running script wth no acceleration 8

--- ci/scripts/emulator.sh
+++ ci/scripts/emulator.sh
@@ -9,10 +9,10 @@ function config_emulator_settings() {
 }
 
 function wait_emulator_to_be_ready() {
-  adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
   adb root
   adb shell avbctl disable-verification
   adb disable-verity
+  adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
   emulator -avd "test" -verbose -no-boot-anim -no-snapshot -no-window -accel off -gpu off -skin 1440x2880 -noaudio -debug-all &
   boot_completed=false
   while [ "$boot_completed" == false ]; do
